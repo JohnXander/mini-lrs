e847a3108df6af931d8cfc4ae10ead3b823fb99a
John Bloxam <johnblox5@gmail.com>
2023-12-18 16:02:02 +0000

    feat(UI): create pages and routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home.jsx';
+import SignIn from "./pages/SignIn.js";
+import SignUp from "./pages/SignUp.js";
+import About from "./pages/About.js";
+import Profile from "./pages/Profile.js";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
