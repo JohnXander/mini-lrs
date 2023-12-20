db58d6fb748ae10e4b70068bac89a6121f90cc35
John Bloxam <johnblox5@gmail.com>
2023-12-20 11:17:15 +0000

    feat: create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose
@@ -16,7 +17,10 @@ mongoose
 const app = express();
 const PORT = process.env.PORT;
 
-app.use('/api/user', userRouter)
+app.use(express.json());
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
