b462a8e48e473fd06c6ec9b3d1f2e8da7965cbf3
John Bloxam <johnblox5@gmail.com>
2024-01-02 17:43:30 +0000

    feat: make profile page private

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import SignUp from "./pages/SignUpPage/index.tsx";
 import About from "./pages/About.tsx";
 import Profile from "./pages/Profile.tsx";
 import Header from "./components/Header.tsx";
+import PrivateRoute from "./components/PrivateRoute.tsx";
 
 export default function App() {
   return (
@@ -15,7 +16,9 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
-        <Route path="profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
---
+++ client/src/components/PrivateRoute.tsx
@@ -0,0 +1,9 @@
+import { useSelector } from 'react-redux';
+import { RootState } from '../redux/store';
+import { Outlet, Navigate } from 'react-router-dom';
+
+export default function PrivateRoute(){
+  const { currentUser } = useSelector((state: RootState) => state.user);
+
+  return currentUser ? <Outlet /> : <Navigate to='\sign-in' />;
+}
