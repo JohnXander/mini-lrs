26e0dbea86d0766b9c98fe4a7994f04f70d94cce
John Bloxam <johnblox5@gmail.com>
2024-01-02 17:43:07 +0000

    feat: update header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,11 @@
 import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { RootState } from '../redux/store';
 
 export default function Header() {
+  const { currentUser } = useSelector((state: RootState) => state.user);
+
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -30,10 +34,20 @@ export default function Header() {
               About
             </li>
           </Link>
-          <Link to='/sign-in' >
-            <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>
-              Sign In
-            </li>
+          <Link to='/profile' >
+            {
+              currentUser ? (
+                <img 
+                  // @ts-expect-error: Temporary workaround for incomplete type information
+                  src={currentUser.avatar} alt="profile"
+                  className='rounded-full h-7 w-7 object-cover'
+                />
+              ) : (
+                <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>
+                  Sign In
+                </li>
+              )
+            }
           </Link>
         </ul>
       </div>
