206f5fed35fb7e08fd6155288ece21fbd0924cc0
John Bloxam <johnblox5@gmail.com>
2024-01-22 13:08:10 +0000

    fix: prop typo

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -21,7 +21,7 @@ export default function Header() {
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
           />
-          <FaSearch classname="text-slate-600" />
+          <FaSearch className="text-slate-600" />
         </form>
         <ul className='flex gap-4'>
           <Link to='/' >
