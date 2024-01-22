971e5680a325fa6be16d05bde765ea2162d53845
John Bloxam <johnblox5@gmail.com>
2024-01-22 16:08:23 +0000

    feat: complete image upload functionality

---
+++ client/src/pages/Profile/Profile.types.ts
@@ -0,0 +1,6 @@
+export interface FormData {
+  username?: string;
+  email?: string;
+  password?: string;
+  avatar?: string;
+}
---
+++ client/src/pages/Profile/index.tsx
@@ -0,0 +1,122 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+import { useEffect, useRef, useState } from "react";
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from "../../firebase";
+import { FormData } from "./Profile.types";
+
+export default function Profile() {
+  const fileRef = useRef<HTMLInputElement | null>(null);
+  const { currentUser } = useSelector((state: RootState) => state.user);
+  const [file, setFile] = useState<File | undefined>(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState<FormData>({});
+
+  useEffect(() => {
+    if (file){
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file: File) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_changed', 
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+        console.error(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref)
+          .then((downloadURL) => {
+            setFormData({ ...formData, avatar: downloadURL});
+          })
+      }
+    );
+  }
+
+  return (
+   <div className="p-3 max-w-lg mx-auto">
+    <h1 className="text-3xl font-semibold text-center my-7">
+      Profile
+    </h1>
+    <form className="flex flex-col gap-4">
+      <input 
+        type="file" 
+        ref={fileRef} 
+        accept="image/*"
+        onChange={(e) => {
+          const selectedFile = e.target.files?.[0];
+          if (selectedFile) {
+            setFile(selectedFile);
+          }
+        }}
+        hidden />
+      <img 
+        src={formData.avatar ||currentUser?.avatar} 
+        alt=""
+        className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+        onClick={() => (fileRef.current as HTMLInputElement | null)?.click()}
+      />
+      <p className="text-sm self-center">
+        {
+          fileUploadError ? (
+            <span className="text-red-700">
+              Image Upload Error (image must be less than 2mb)
+            </span>
+          ) : filePerc > 0 && filePerc < 100 ? (
+            <span className="text-slate-700">
+              {`Uploading ${filePerc}%`}
+            </span>
+          ) : filePerc === 100 ? (
+            <span className="text-green-700">
+              Successfully Uploaded!
+            </span>
+          ) : null
+        }
+      </p>
+      <input
+        id="username"
+        type="text"
+        placeholder="username"
+        className="border p-3 rounded-lg"
+      />
+      <input
+        id="email"
+        type="email"
+        placeholder="email"
+        className="border p-3 rounded-lg"
+      />
+      <input
+        id="password"
+        type="password"
+        placeholder="password"
+        className="border p-3 rounded-lg"
+      />
+      <button 
+        className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80"
+      >
+        Update
+      </button>
+    </form>
+    <div className="flex justify-between mt-5">
+      <span 
+        className="text-red-700 cursor-pointer">
+        Delete account
+      </span>
+      <span 
+        className="text-red-700 cursor-pointer">
+        Sign out
+      </span>
+    </div>
+   </div>
+  )
+}
