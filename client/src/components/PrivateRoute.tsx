07f0c3ba162c994410c2d48002b25f38b2b86ebe
John Bloxam <johnblox5@gmail.com>
2024-01-23 17:03:17 +0000

    fix: change back slash to forward slash

--- client/src/components/PrivateRoute.tsx
+++ client/src/components/PrivateRoute.tsx
@@ -5,5 +5,5 @@ import { Outlet, Navigate } from 'react-router-dom';
 export default function PrivateRoute(){
   const { currentUser } = useSelector((state: RootState) => state.user);
 
-  return currentUser ? <Outlet /> : <Navigate to='\sign-in' />;
+  return currentUser ? <Outlet /> : <Navigate to='/sign-in' />;
 }
