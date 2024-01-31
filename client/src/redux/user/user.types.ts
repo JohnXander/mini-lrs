9bf3d6aa789b13a6247972a3b05831b975408e69
John Bloxam <johnblox5@gmail.com>
2024-01-31 17:27:39 +0000

    fix: resolve deployment bug

--- client/src/redux/user/user.types.ts
+++ client/src/redux/user/user.types.ts
@@ -8,7 +8,13 @@ interface User {
 }
 
 export interface UserState {
-  currentUser: User | null;
+  currentUser: {
+    _id: string;
+    username: string;
+    email: string;
+    password: string;
+    avatar: string;
+  } | null;
   error: string | null;
   loading: boolean;
   allUsers: User[];
