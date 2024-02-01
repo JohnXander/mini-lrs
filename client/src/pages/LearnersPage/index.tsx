3b825ecd1ded8b48e542eb80e39173d1e142592b
John Bloxam <johnblox5@gmail.com>
2024-02-01 15:04:53 +0000

    feat: add avatar and createdAt to learner item

--- client/src/pages/LearnersPage/index.tsx
+++ client/src/pages/LearnersPage/index.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { fetchUsersFailure, fetchUsersStart, fetchUsersSuccess } from "../../redux/user/userSlice";
 import { User } from "./Learners.types";
 import { LearnerModal } from "./components/LearnerModal";
+import { formatDistanceToNow } from 'date-fns';
 
 export default function Learners() {
   const { allUsers, loading, error } = useSelector((state: RootState) => state.user);
@@ -50,15 +51,30 @@ export default function Learners() {
       {error && <p>Error: {error}</p>}
       {!loading && !error && (
         <ul className='flex flex-col gap-1'>
-          {allUsers.map((user, index) => (
+          {allUsers.slice().reverse().map((user, index) => (
             <li 
-              className='flex justify-between gap-2 border border-slate-200 p-2 hover:cursor-pointer hover:bg-slate-200' 
+              className='flex justify-between items-center gap-2 border border-slate-200 p-2 hover:cursor-pointer hover:bg-slate-200 flex-wrap' 
               key={index}
               onClick={() => {
                 setSelectedUser(user);
                 setIsModalOpen(true);
               }}>
-              {user.username}
+              <div className='flex items-center gap-3'>
+                <img 
+                  src={user.avatar} 
+                  className='w-8 h-8 rounded-full object-cover'
+                />
+                <p 
+                  className='text-slate-700 max-w-[200px] overflow-hidden overflow-ellipsis whitespace-nowrap'
+                  title={user._id}>
+                  {user.username}
+                </p>
+              </div>
+              <p 
+                className='text-gray-500'
+                title={new Date(user.createdAt).toLocaleString()}>
+                {`joined ${formatDistanceToNow(new Date(user.createdAt), { addSuffix: true })}`}
+              </p>
             </li>
           ))}
         </ul>
