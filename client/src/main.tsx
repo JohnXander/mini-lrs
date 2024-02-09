c610288128e6151f7f3b4a9dc555a84d696176af
John Bloxam <johnblox5@gmail.com>
2024-02-09 16:58:11 +0000

    fix: resolve react-modal console warning

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,9 @@ import './index.css';
 import { persistor, store } from './redux/store.ts'
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import Modal from 'react-modal';
+
+Modal.setAppElement(document.body);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
