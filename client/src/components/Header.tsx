a3ae2d2efb8cf06f941a4f6f63fb495ccc7fce5a
John Bloxam <johnblox5@gmail.com>
2024-02-11 13:59:58 +0000

    feat: make header responsive

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFileLines, faUsers, faPlay } from '@fortawesome/free-solid-svg-icons'
 
 export default function Header() {
   const { currentUser } = useSelector((state: RootState) => state.user);
@@ -9,25 +11,28 @@ export default function Header() {
     <header className="bg-slate-200 shadow-md py-3">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to='/' >
-          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+          <h1 className="font-bold text-lg sm:text-xl flex flex-wrap">
             <span className="text-slate-500">Mini</span>
             <span className="text-slate-700">LRS</span>
           </h1>
         </Link>
-        <ul className='flex gap-4'>
+        <ul className='flex gap-5'>
           <Link to='/' >
-            <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>
-              Statements
+            <li className='text-slate-700 hover:underline cursor-pointer'>
+              <FontAwesomeIcon className='sm:hidden' icon={faFileLines} />
+              <span className='hidden sm:inline-block'>Statements</span>
             </li>
           </Link>
           <Link to='/learners' >
-            <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>
-              Learners
+            <li className='text-slate-700 hover:underline cursor-pointer'>
+              <FontAwesomeIcon className='sm:hidden' icon={faUsers} />
+              <span className='hidden sm:inline-block'>Learners</span>
             </li>
           </Link>
           <Link to='/demo' >
-            <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>
-              Demo
+            <li className='text-slate-700 hover:underline cursor-pointer'>
+              <FontAwesomeIcon className='sm:hidden' icon={faPlay} />
+              <span className='hidden sm:inline-block'>Demo</span>
             </li>
           </Link>
           <Link to='/profile' >
